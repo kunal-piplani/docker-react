87a25ba363e1c420ea53999d727d6f5dc95355b7
kunal-piplani <kunal.piplani12@gmail.com>
2020-07-27 00:37:19 +0530

    adde file

--- src/App.test.js
+++ src/App.test.js
@@ -6,10 +6,8 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
